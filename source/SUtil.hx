84cdf473c05aba5e5fee88089a4363860ee3c8f1
DANIZIN <98532797+DANIZIN23@users.noreply.github.com>
2022-10-17 20:22:39 -0300

    Update SUtil.hx

--- source/SUtil.hx
+++ source/SUtil.hx
@@ -1,7 +1,7 @@
 package;
 
 #if android
-import android.Androidtools;
+import android.AndroidTools;
 import android.Permissions;
 #end
 import lime.app.Application;
@@ -15,7 +15,7 @@ class SUtil
 {
     #if android
     private static var aDir:String = null;
-    private static var sPath:String = Androidtools.getExternalStorageDirectory();  
+    private static var sPath:String = AndroidTools.getExternalStorageDirectory();  
     private static var grantedPermsList:Array<Permissions> = AndroidTools.getGrantedPermissions();  
     #end
 
